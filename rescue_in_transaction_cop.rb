698d0afda105b95e20367e7cd4d07c4b1cb91dc9
Gaku Nakagawa <gaku@siderlabs.com>
2021-12-02 00:36:16 +0900

    update

--- rescue_in_transaction_cop.rb
+++ rescue_in_transaction_cop.rb
@@ -8,7 +8,7 @@ class RuboCop::Cop::Lint::RescueInTransaction < RuboCop::Cop::Base
     )
   PATTERN
   
-  MSG = 'Avoid the use of `rescue` in a `ApplicationRecord.transaction` block'.freeze
+  MSG = 'Avoid the use of `rescue` in a `transaction` block'.freeze
   
   def on_block(node)
     return unless violate?(node)
