43e9d723ef4e4c205a84f1bcb3c0ea2a30b66740
Gaku Nakagawa <gaku@siderlabs.com>
2022-09-04 04:00:08 +0900

    add

---
+++ rescue_in_transaction_cop.rb
@@ -0,0 +1,18 @@
+class RuboCop::Cop::Lint::RescueInTransaction < RuboCop::Cop::Base
+  def_node_matcher :violate?, <<~PATTERN
+    (block
+      (send
+        _ :transaction)
+      (args)
+      `(rescue ...)
+    )
+  PATTERN
+  
+  MSG = 'Avoid the use of `rescue` in a `transaction` block'.freeze
+  
+  def on_block(node)
+    return unless violate?(node)
+    
+    add_offense(node)
+  end
+end
